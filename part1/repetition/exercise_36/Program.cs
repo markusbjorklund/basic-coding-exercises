4147a7990c9de97002a1178cccaca19f4a55d49e
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-01-23 18:35:37 +0200

    Exercise 36 done

--- part1/repetition/exercise_36/Program.cs
+++ part1/repetition/exercise_36/Program.cs
@@ -7,7 +7,24 @@ namespace exercise_36
     public static void Main(string[] args)
     {
       // Write your code here:
-
+      while (true) 
+      {
+        Console.WriteLine("Give a number:");
+        string userInput = Console.ReadLine();
+        int userNumber = Convert.ToInt32(userInput);
+        if (userNumber == 0)
+        { 
+          break;
+        }
+        else if (userNumber < 0)
+        {
+          Console.WriteLine("That is negative");
+        }
+        else if (userNumber > 0)
+        {
+          Console.WriteLine(userNumber * userNumber);
+        }
+      }
     }
   }
 }
