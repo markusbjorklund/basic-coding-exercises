1dbd87dc3eb528b797bb94dcf9a18401571de348
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-01-23 17:38:14 +0200

    Exercise 27 done

--- part1/conditionals/exercise_27/Program.cs
+++ part1/conditionals/exercise_27/Program.cs
@@ -7,7 +7,17 @@ namespace exercise_27
     public static void Main(string[] args)
     {
       // Write your code here:
-
+      Console.WriteLine("Give a number:");
+      string userInput = Console.ReadLine();
+      int intNumber = Convert.ToInt32(userInput);
+      if (intNumber > 0)
+      {
+        Console.WriteLine("It is positive");
+      }
+      else 
+      {
+        Console.WriteLine("It is not positive");
+      }
     }
   }
 }
