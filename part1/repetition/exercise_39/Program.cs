4b97fad7c193464b7243adae27dcaa2782211cb1
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-01-23 19:23:39 +0200

    Exercise 35 done

--- part1/repetition/exercise_39/Program.cs
+++ part1/repetition/exercise_39/Program.cs
@@ -7,6 +7,22 @@ namespace exercise_39
     public static void Main(string[] args)
     {
       // Write your code here:
+      int countNumber = 0;
+      while (true) 
+      {
+        Console.WriteLine("Give a number:");
+        string userInput = Console.ReadLine();
+        int userNumber = Convert.ToInt32(userInput);
+        if (userNumber == 0)
+        { 
+          break;
+        }
+        if (userNumber > 0)
+        {
+          countNumber = countNumber +1;
+        }  
+      }
+      Console.WriteLine("Total sum of numbers: " + countNumber);
     }
   }
 }
