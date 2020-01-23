c59ca8ced74592fb1287cb18c4567be8e7c95d6f
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-01-23 19:34:13 +0200

    Exercise 40 done

--- part1/repetition/exercise_40/Program.cs
+++ part1/repetition/exercise_40/Program.cs
@@ -8,6 +8,24 @@ namespace exercise_40
     {
 
       // Write your code here:
+      int sumNumbers = 0;
+      int countNumbers = 0;
+      while (true) 
+      {
+        Console.WriteLine("Give a number:");
+        int userNumber = Convert.ToInt32(Console.ReadLine());
+        if (userNumber == 0)
+        { 
+          break;
+        }
+        else
+        {
+          sumNumbers = sumNumbers + userNumber;
+          countNumbers = countNumbers +1;
+        }  
+      }
+      Console.WriteLine("Total sum of numbers: " + sumNumbers);
+      Console.WriteLine("Total amount of numbers: " + countNumbers);
     }
   }
 }
