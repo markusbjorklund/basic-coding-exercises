e75f270c353da464f405c0b56441be73139be5cf
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-01-23 17:32:42 +0200

    Exercise 20 done

--- part1/calculations/exercise_20/Program.cs
+++ part1/calculations/exercise_20/Program.cs
@@ -8,7 +8,13 @@ namespace exercise_20
     {
 
       // Write your code here:
-
+      Console.WriteLine("Give the first number!");
+      string numFirst = Console.ReadLine();
+      int intValueFirst = Convert.ToInt32(numFirst);
+      Console.WriteLine("Give the second number!");
+      string numSecond = Console.ReadLine();
+      int intValueSecond = Convert.ToInt32(numSecond);
+      Console.WriteLine(intValueFirst + " * " + intValueSecond + " = " + (intValueFirst * intValueSecond));
     }
   }
 }
