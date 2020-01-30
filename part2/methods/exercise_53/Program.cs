afc825e633193849af50675fb3c466b23f17be4b
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-01-30 13:47:41 +0200

    Exercise 53 done

--- part2/methods/exercise_53/Program.cs
+++ part2/methods/exercise_53/Program.cs
@@ -7,11 +7,13 @@ namespace exercise_53
     public static void Main(String[] args)
     {
       // Call your method here:
-
-
+      Division(10, 2);
     }
 
     // Write your method here:
-
+    public static void Division(int numerator, int denominator)
+    {
+      Console.WriteLine((double)numerator / denominator);
+    }
   }
 }
