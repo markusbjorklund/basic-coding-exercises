5360ff2b1e5a382a1ca8eb2e56bab0fe3b8d0792
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-01-30 13:11:52 +0200

    Exercise 50 done

--- part2/methods/exercise_50/Program.cs
+++ part2/methods/exercise_50/Program.cs
@@ -7,12 +7,21 @@ namespace exercise_50
     public static void Main(string[] args)
     {
       // Call your method here:
- 
+      Console.WriteLine("How many times?");
+      int numOfTimes = Convert.ToInt32(Console.ReadLine());
+
+      int i = 0;
+      while (i < numOfTimes)
+      {
+        PrintPhrase();
+        i++;
+      }
     }
 
     // Write your method here:
-    public static void PrintPhrase() {
-
+    public static void PrintPhrase() 
+    {
+      Console.WriteLine("In a hole in the ground there lived a method");
     }
   }
 }
