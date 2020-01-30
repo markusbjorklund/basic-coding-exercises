66f688757aea83adff4ee5e7f886841bc5c77218
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-01-30 14:49:48 +0200

    Exercise 57 done

--- part2/methods/exercise_57/Program.cs
+++ part2/methods/exercise_57/Program.cs
@@ -4,16 +4,17 @@ namespace exercise_57
 {
   class Program
   {
-    public static int Sum(int number1, int number2, int number3, int number4)
-    {
-      // write your code here
-      // remember to include return (at the end)!
-    }
-
-    public static void Main(String[] args)
+    public static void Main(String[] args) 
     {
       int answer = Sum(4, 3, 6, 1);
       Console.WriteLine("Sum: " + answer);
+    }  
+    public static int Sum(int number1, int number2, int number3, int number4) 
+    {
+      // write your code here
+      // remember to include return (at the end)!
+      int answer = number1 + number2 + number3 + number4;
+      return answer;
     }
   }
 }
