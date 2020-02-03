7b1f45ad073ed995e5a1ff70d20b7f814e27ecdf
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-02-03 13:03:34 +0200

    Exercise 62 done

--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace sandbox
 {
@@ -7,6 +8,26 @@ namespace sandbox
         static void Main(string[] args)
         {
 
+List<int> numbers = new List<int>();
+
+numbers.Add(1);
+numbers.Add(2);
+numbers.Add(3);
+numbers.Add(4);
+
+for (int i - 1; i <= numbers.Count; i--)
+    {
+        int number = numbers[i];
+        Console.WriteLine(number);
+    }
+
+//int index = numbers.Count - 1;
+//while (index >= 0)
+//{
+//  int number = numbers[index];
+//  Console.WriteLine(number);
+//  index = index - 1;
+//}
         
         }
     }
