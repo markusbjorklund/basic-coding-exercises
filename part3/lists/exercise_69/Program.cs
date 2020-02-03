1dddd890936390db7318cf29409c92bdec202b08
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-02-03 21:38:17 +0200

    Exercise 70 done

--- part3/lists/exercise_69/Program.cs
+++ part3/lists/exercise_69/Program.cs
@@ -16,7 +16,24 @@ namespace exercise_69
           break;
         }
         list.Add(input);
-      }
+        }
+        Console.WriteLine("From where?");
+        int start = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine(start);
+
+        Console.WriteLine("Where to?");
+        int end = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(end);
+
+        // if (list.Contains(start))
+        // {
+        //   Console.WriteLine("This is the number " + start);
+        //   list.ForEach(Console.WriteLine);
+        // }
+
+
 
     }
   }
