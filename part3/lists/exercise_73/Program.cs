2e8bc4743366affd2b84b51777e0fdd6d13e05eb
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-02-05 16:25:51 +0200

    Exercise 73 done

--- part3/lists/exercise_73/Program.cs
+++ part3/lists/exercise_73/Program.cs
@@ -1,23 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace exercise_73
-{
-  class Program
-  {
-    public static void Main(string[] args)
-    {
-      List<int> list = new List<int>();
-      while (true)
-      {
-        int input = Convert.ToInt32(Console.ReadLine());
-        if (input == -1)
-        {
+namespace exercise_73 {
+  class Program {
+    public static void Main (string[] args) {
+      List<int> list = new List<int> ();
+      while (true) {
+        int input = Convert.ToInt32 (Console.ReadLine ());
+        if (input == -1) {
           break;
         }
-        list.Add(input);
+        list.Add (input);
       }
 
+      // start from scratch
+      int sum = 0;
+
+      // count the numbers
+      for (int i = 0; i < list.Count; i++) {
+        int number = list[i];
+        sum = sum + number;
+      }
+      Console.WriteLine ("Sum: " + sum);
     }
   }
 }
