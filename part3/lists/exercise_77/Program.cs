e92d71c30e9661898f17fecea418f8cd46021dd5
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-02-05 19:07:41 +0200

    Exercise 77 done

--- part3/lists/exercise_77/Program.cs
+++ part3/lists/exercise_77/Program.cs
@@ -1,16 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace exercise_77
-{
-  class Program
-  {
-    public static void Main(string[] args)
-    {
+namespace exercise_77 {
+  class Program {
+    public static void Main (string[] args) {
+      List<string> strings = new List<string> ();
+
+      strings.Add ("First");
+      strings.Add ("Second");
+      strings.Add ("Third");
+
+      // Remember, this is how you print all the items in a list
+      strings.ForEach (Console.WriteLine);
+
+      RemoveLast (strings);
+      RemoveLast (strings);
+
+      strings.ForEach (Console.WriteLine);
+
       // You can try your method here if you want
     }
 
+    // method
+    public static void RemoveLast (List<string> strings) {
+
+      // find last string value
+      int lastValue = strings.Count -1;
+
+      // and remove it
+      strings.RemoveAt (lastValue);
+    }
   }
-}
-
-
+} 
