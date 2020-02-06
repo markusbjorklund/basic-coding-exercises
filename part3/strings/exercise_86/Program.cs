6caa22efc75b21c43d1807bb1b2797aa6eeddcaa
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-02-06 13:28:33 +0200

    Exercise 84 done

--- part3/strings/exercise_86/Program.cs
+++ part3/strings/exercise_86/Program.cs
@@ -7,9 +7,24 @@ namespace exercise_86
   {
     public static void Main(string[] args)
     {
+      // read from user
+      while (true)
+      {
+        string asked = Console.ReadLine();
+        //break if empty
+        if (asked == "")
+        {
+          break;
+        }
+        // string "asked" and split from white space
+        // create array called "parts"
+        string[] parts = asked.Split(" ");
+        foreach (string line in parts)
+        {
+          Console.WriteLine(line);
+        }
 
-
+      }
     }
   }
 }
-
