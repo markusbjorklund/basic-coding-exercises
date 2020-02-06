6db97405ff673c24603016655d4979a501d8e2f3
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-02-06 11:45:23 +0200

    Exercise 82 done

--- part3/arrays/exercise_82/Program.cs
+++ part3/arrays/exercise_82/Program.cs
@@ -10,13 +10,23 @@ namespace exercise_82
       // You can test your method here
       int[] array = { 5, 1, 3, 4, 2 };
       PrintArrayInStars(array);
-
     }
 
     public static void PrintArrayInStars(int[] array)
     {
+      // list the array
+      for (int i = 0; i < array.Length; i++)
+      {
+        // number of stars
+        for (int stars = 0; stars < array[i]; stars++)
+        {
+          // convert to stars
+          Console.Write("*");
+        }
+        // give me some linebreaks
+        Console.WriteLine();
+      }
 
     }
   }
 }
-
