914b17fb966cec51d71fb103d6eec18bc8271f48
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-02-06 13:07:28 +0200

    Exercise 83 done

--- part3/strings/exercise_90/Program.cs
+++ part3/strings/exercise_90/Program.cs
@@ -7,11 +7,37 @@ namespace exercise_90
   {
     public static void Main(string[] args)
     {
+      List<string> persons = new List<string>();
+      //int oldest
+      int oldest = 0;
+      // read from user
+      while (true)
+      {
+        string asked = Console.ReadLine();
+        //break if empty
+        if (asked == "")
+        {
+          break;
+        }
+        
+        // split strings into parts[0] and parts[1]
+        // split strings into name and age
+        string[] parts = asked.Split(",");
 
+        //check if age is greater than oldest
+        if (Convert.ToInt32(parts[1]) > oldest)
+        {
+          //if true oldest is now parts[1] value
+          oldest = Convert.ToInt32(parts[1]);
+        }
+      }
+      // print the oldest
+      Console.WriteLine("Age of the oldest: " + oldest);
 
+      foreach (string person in persons)
+      {
+        Console.WriteLine(person);
+      }
     }
   }
 }
-
-
-
