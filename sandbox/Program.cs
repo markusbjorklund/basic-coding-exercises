36ae9e203573fdee81f5dde75df9273553106153
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-02-10 18:52:42 +0200

    Exercise 98 done

--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -12,11 +12,13 @@ namespace sandbox
       Person matti = new Person("Matti");
       Person juhana = new Person("Juhana");
 
-      matti.height = 180;
+      matti.height = 1.1;
       matti.weight = 86;
 
       juhana.height = 175;
       juhana.weight = 64;
+
+      Console.WriteLine(matti.name + " ---- " + matti.height + " ------ " + juhana.weight + " -----" + ", body mass index is ");
 
       Console.WriteLine(matti.name + ", body mass index is " + matti.BodyMassIndex());
       Console.WriteLine(juhana.name + ", body mass index is " + juhana.BodyMassIndex());
