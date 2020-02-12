c9fb573870e0aea1c01a4bb8de621dd61acc34d5
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-02-13 01:14:37 +0200

    Exercise 113 done

--- part4/objectlist/exercise_113/Program.cs
+++ part4/objectlist/exercise_113/Program.cs
@@ -3,15 +3,58 @@ using System.Collections.Generic;
 
 namespace exercise_113
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // IMPLEMENT YOUR CODE IN HERE!
-      // Remember also to create the file Book.cs and class in it!
+        public static void Main(string[] args)
+        {
+            // IMPLEMENT YOUR CODE IN HERE!
+            // Remember also to create the file Book.cs and class in it!
+            List<Book> books = new List<Book>();
 
+            while (true)
+            {
+                // ask for title 
+                Console.Write("Name: ");
+                string title = Console.ReadLine();
+                if (title == "")
+                {
+                    break;
+                }
+                // ask for amount of pages
+                Console.Write("Pages: ");
+                int pages = Convert.ToInt32(Console.ReadLine());
+                // ask for publication year
+                Console.Write("Publication year: ");
+                int year = Convert.ToInt32(Console.ReadLine());
+
+                // add bookinfo to list
+                books.Add(new Book(title, pages, year));
+            }
+
+            // tidy up the print with a blank line
+            Console.WriteLine();
+            // what do you want to see?
+            Console.Write("What information will be printed? ");
+            string input = Console.ReadLine();
+            if (input == "name")
+            {
+                foreach (Book item in books)
+                {
+                    // just print the title
+                    Console.WriteLine(item.title);
+                }
+            }
+
+            else if (input == "everything")
+            {
+                foreach (Book item in books)
+                {
+                    // print everything
+                    Console.WriteLine(item);
+                }
+            }
+        }
     }
-  }
 }
 
 
