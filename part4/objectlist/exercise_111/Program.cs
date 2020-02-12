4d4085b43c3d5bff80410a3b69871d6208724c49
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-02-12 19:59:18 +0200

    Exercise 111 done

--- part4/objectlist/exercise_111/Program.cs
+++ part4/objectlist/exercise_111/Program.cs
@@ -3,15 +3,42 @@ using System.Collections.Generic;
 
 namespace exercise_111
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // IMPLEMENT YOUR CODE IN HERE!
-      List<PersonalInformation> list = new List<PersonalInformation>();
+        public static void Main(string[] args)
+        {
+            // IMPLEMENT YOUR CODE IN HERE!
+            List<PersonalInformation> list = new List<PersonalInformation>();
+            while (true)
+            {
+                // first name
+                Console.WriteLine("First name:");
+                string firstName = Console.ReadLine();
+                // break on empty first name
+                if (firstName == "")
+                {
+                    break;
+                }
+                // last name
+                Console.WriteLine("Last name:");
+                string lastName = Console.ReadLine();
+                // id number
+                Console.WriteLine("Identification number:");
+                string identificationNumber = Console.ReadLine();
 
+                // add names to the list
+                list.Add(new PersonalInformation(firstName, lastName, identificationNumber));
+            }
+            // tidy up with an empty line
+            Console.WriteLine();
+
+            // print what we want
+            foreach (PersonalInformation line in list)
+            {
+                Console.WriteLine(line.firstName + " " + line.lastName);
+            }
+        }
     }
-  }
 }
 
 
