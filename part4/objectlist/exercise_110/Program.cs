d8e9180fc9d255aafc7569066b7621783dfe941c
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-02-12 18:21:26 +0200

    Exercise 110 done

--- part4/objectlist/exercise_110/Program.cs
+++ part4/objectlist/exercise_110/Program.cs
@@ -8,10 +8,28 @@ namespace exercise_110
         public static void Main(string[] args)
         {
             // IMPLEMENT YOUR CODE IN HERE!
+            // note to self, it's called Item.cs
+            // worked with list named list but failed testing
+            // fucker should be called items
+            List<Item> items = new List<Item>();
+            while (true)
+            {
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                if (name == "")
+                {
+                    break;
+                }
+                // add names to the list
+                items.Add(new Item(name));
+            }
+            // tidy up with an empty line
+            Console.WriteLine();
+            // show me the names and time
+            foreach (Item name in items)
+            {
+                Console.WriteLine(name);
+            }
         }
     }
 }
-
-
-
-
