6d67be60bb45e4627bacbe36c4041cf580d39aaa
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-02-13 10:50:47 +0200

    Enter Sandbox

--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -1,29 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 
 namespace sandbox
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      // testarea
 
-      Person matti = new Person("Matti");
-      Person juhana = new Person("Juhana");
+// play in the sand
 
-      matti.height = 1.1;
-      matti.weight = 86;
+        static void Main(string[] args)
+        {
+            // Example #1
+            // Read the file as one string.
+            string text = File.ReadAllText("text.txt");
 
-      juhana.height = 175;
-      juhana.weight = 64;
+            // Display the file contents to the console. Variable text is a string.
+            Console.WriteLine("This was done with ReadAllText.");
+            Console.WriteLine(text);
 
-      Console.WriteLine(matti.name + " ---- " + matti.height + " ------ " + juhana.weight + " -----" + ", body mass index is ");
+            //Print empty line for easier reading
+            Console.WriteLine();
 
-      Console.WriteLine(matti.name + ", body mass index is " + matti.BodyMassIndex());
-      Console.WriteLine(juhana.name + ", body mass index is " + juhana.BodyMassIndex());
+            // Example #2
+            // Read each line of the file into a string array. 
+            //Each element of the array is one line of the file.
+            Console.WriteLine("This was done with ReadAllLines.");
+            string[] lines = File.ReadAllLines("text.txt");
+
+            // Display the file contents by using a foreach loop.
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+// stop playing
 
     }
-    // testarea
-  }
 }
