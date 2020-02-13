58b4e6443fb84f293023ae54dccf8977942ffe74
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-02-13 12:35:28 +0200

    Exercise 114 done

--- part4/files/exercise_114/Program.cs
+++ part4/files/exercise_114/Program.cs
@@ -6,17 +6,22 @@ namespace exercise_114
     {
         public static void Main(string[] args)
         {
-            
+
+          // start counter
+          int counter = 0;
             while (true)
             {
                 string line = Console.ReadLine();
+                // count lines
+                counter++;
 
                 if (line == "end")
                 {
                     break;
                 }
             }
-            Console.WriteLine(line.Count());
+            // print number of lines
+            Console.WriteLine(counter -1);
         }
     }
 }
