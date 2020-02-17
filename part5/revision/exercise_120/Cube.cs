3259094187e4962fbbf907ec0179f08f9d0c7de0
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-02-17 14:18:12 +0200

    Exercise 120 done

--- part5/revision/exercise_120/Cube.cs
+++ part5/revision/exercise_120/Cube.cs
@@ -3,17 +3,18 @@ namespace exercise_120
   public class Cube
   {
     private int edgeLength;
+    private int volume;
     public Cube(int edgeLength)
     {
       this.edgeLength = edgeLength;
     }
     public int Volume()
     {
-      return this.edgeLength * this.edgeLength * this.edgeLength;
+      return this.volume = this.edgeLength * this.edgeLength * this.edgeLength;
     }
     public override string ToString()
     {
-      return "The length of the edge is " + this.edgeLength + " and the volume ";
+      return "The length of the edge is " + this.edgeLength + " and the volume " + this.Volume();
     }
   }
 }
