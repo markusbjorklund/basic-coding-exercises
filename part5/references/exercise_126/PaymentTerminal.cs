0b70fe2d5e37a0ff886c2e4aedfbeef022a8d1d0
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-02-18 19:53:00 +0200

    Exercise 126 done

--- part5/references/exercise_126/PaymentTerminal.cs
+++ part5/references/exercise_126/PaymentTerminal.cs
@@ -9,6 +9,9 @@ namespace exercise_126
     public PaymentTerminal()
     {
       // register initially has 1000 euros of money
+      this.money = 1000;
+      this.coffeeAmount = 0;
+      this.lunchAmount = 0;
     }
 
     public double DrinkCoffee(double payment)
@@ -16,8 +19,13 @@ namespace exercise_126
       // an coffee now costs 2.50 euros
       // increase the amount of cash by the price of an coffee mean and return the change
       // if the payment parameter is not large enough, no coffee is sold and the method should return the whole payment
-
-      return 0;
+      if (payment >= 2.50)
+      {
+        this.money += 2.50;
+        this.coffeeAmount++;
+        return payment - 2.50;
+      }
+      return payment;
     }
 
     public double EatLunch(double payment)
@@ -25,8 +33,13 @@ namespace exercise_126
       // a lunch now costs 10.30 euros
       // increase the amount of cash by the price of a lunch and return the change
       // if the payment parameter is not large enough, no lunch is sold and the method should return the whole payment
-
-      return 0;
+      if (payment >= 10.30)
+      {
+        this.money += 10.30;
+        this.lunchAmount++;
+        return payment - 10.30;
+      }
+      return payment;
     }
 
     public bool DrinkCoffee(PaymentCard card)
@@ -34,6 +47,11 @@ namespace exercise_126
       // a coffee costs 2.50 euros
       // if the payment card has enough money, the balance of the card is decreased by the price, and the method returns true
       // otherwise false is returned
+      if (card.balance >= 2.50)
+      {
+        card.TakeMoney(2.50);
+        return true;
+      }
 
       return false;
     }
@@ -43,13 +61,18 @@ namespace exercise_126
       // a lunch costs 10.30 euros
       // if the payment card has enough money, the balance of the card is decreased by the price, and the method returns true
       // otherwise false is returned
+      if (card.balance >= 10.30)
+      {
+        card.TakeMoney(10.30);
+        return true;
+      }
       return false;
     }
 
     public void AddMoneyToCard(PaymentCard card, double sum)
     {
-      // ...
-
+      card.AddMoney(sum);
+      this.money = this.money + sum;
     }
 
     public override string ToString()
