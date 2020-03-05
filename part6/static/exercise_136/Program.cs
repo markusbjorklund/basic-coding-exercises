c8dc847e9f122c1ae13879e46bde578fe9ab80f3
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-03-05 19:07:51 +0200

    Exercise 136 done

--- part6/static/exercise_136/Program.cs
+++ part6/static/exercise_136/Program.cs
@@ -17,5 +17,10 @@ namespace exercise_136
     }
 
     // Do something here
+    public static void HowManyNames(Person person)
+    {
+      // Split the name (treated as an object) on space, print how many names (objects)
+      Console.WriteLine(person + " has " + person.ToString().Split(" ").Length + " names.");
+    }
   }
 }
