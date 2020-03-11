7df20938192f6917f6368d2317cd4bd9d6f849e1
markusbjorklund <markus.bjorklund@gmail.com>
2020-03-11 19:18:51 +0200

    fixed typo

--- extra/extra_02/Program.cs
+++ extra/extra_02/Program.cs
@@ -8,12 +8,12 @@ namespace extra_02
     {
       // Add your code here:
 
-      // ask user for three numbers:
+      // ask user for three numbers
       Console.WriteLine("Give three numbers:");
       int numOne = Convert.ToInt32(Console.ReadLine());
       int numTwo = Convert.ToInt32(Console.ReadLine());
       int numThree = Convert.ToInt32(Console.ReadLine());
-      // find the biggest number and print it
+      // find the largest number and print it
       if (numOne > numTwo && numOne > numThree)
       {
         Console.WriteLine("Largest: " + numOne);
