87672c8fa8d66e3358f1bc014bd7e28ccf6afdc2
markusbjorklund <markus.bjorklund@gmail.com>
2020-03-11 17:35:26 +0200

    changed from while loop to a for loop.

--- extra/extra_01/Program.cs
+++ extra/extra_01/Program.cs
@@ -1,30 +1,41 @@
 ﻿using System;
 
-namespace extra_01 {
-  class Program {
-    public static void Main(string[] args) {
-      // Add your code here:
+namespace extra_01
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Add your code here:
 
-      // ask user for input
-      Console.WriteLine("What do you want to print?");
-      // user inputs text
-      string userInput = Console.ReadLine();
-      // ask user for how many times
-      Console.WriteLine("How many times do you want to print?");
-      // a million of times says user  
-      int numOfTimes = Convert.ToInt32(Console.ReadLine());
+            // ask user for input
+            Console.WriteLine("What do you want to print?");
+            // user inputs text
+            string userInput = Console.ReadLine();
+            // ask user for how many times
+            Console.WriteLine("How many times do you want to print?");
+            // a million of times says user  
+            int numOfTimes = Convert.ToInt32(Console.ReadLine());
 
-      // print what the method does to console
-      PrintThis(numOfTimes, userInput);
+            // print to console
+            PrintMethod(numOfTimes, userInput);
+        }
+
+        public static void PrintMethod(int numOfTimes, string userInput)
+        {
+            // loop numOfTimes 
+            // int i = 0;
+            // while (i < numOfTimes)
+            // {
+            //     Console.WriteLine(userInput);
+            //     i++;
+            // }
+
+            // use for instead of while
+            for (int i = 0; i < numOfTimes; i++)
+            {
+              Console.WriteLine(userInput);
+            } 
+        }
     }
-
-    public static void PrintThis(int numOfTimes, string userInput) {
-      // loop numOfTimes 
-      int i = 0;
-      while (i < numOfTimes) {
-        Console.WriteLine(userInput);
-        i++;
-      }
-    }
-  }
 }
