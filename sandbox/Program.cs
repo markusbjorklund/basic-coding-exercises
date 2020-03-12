d89243296f2405addf05a571bf79bbc373da4b86
markusbjorklund <markus.bjorklund@gmail.com>
2020-03-12 11:15:24 +0200

    just some testing

--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -1,40 +1,28 @@
 ﻿using System;
-using System.IO;
+using System.Collections.Generic;
 
-namespace sandbox
+namespace exercise_136
 {
-    class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
+      Person ada = new Person("Ada Lovelace");
+      Person jack = new Person("Jack The Ripper");
+      Person mike = new Person("Mike The Incredible Magic Mouse");
 
-// play in the sand
-
-        static void Main(string[] args)
-        {
-            // Example #1
-            // Read the file as one string.
-            string text = File.ReadAllText("text.txt");
-
-            // Display the file contents to the console. Variable text is a string.
-            Console.WriteLine("This was done with ReadAllText.");
-            Console.WriteLine(text);
-
-            //Print empty line for easier reading
-            Console.WriteLine();
-
-            // Example #2
-            // Read each line of the file into a string array. 
-            //Each element of the array is one line of the file.
-            Console.WriteLine("This was done with ReadAllLines.");
-            string[] lines = File.ReadAllLines("text.txt");
-
-            // Display the file contents by using a foreach loop.
-            foreach (string line in lines)
-            {
-                Console.WriteLine(line);
-            }
-        }
-
-// stop playing
-
+      HowManyNames(ada);
+      HowManyNames(jack);
+      HowManyNames(mike);
     }
+
+    // Do something here
+    public static void HowManyNames(Person name)
+    {
+     
+      string[] parts = name.ToString().Split(" ");
+     
+      Console.WriteLine(name.ToString() + " has " + parts.Length + " names.");
+    }
+  }
 }
