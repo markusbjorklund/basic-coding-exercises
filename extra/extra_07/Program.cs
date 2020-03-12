57474a1a79d2e2283e8a7608d2b70cb5b200ed7b
markusbjorklund <markus.bjorklund@gmail.com>
2020-03-12 21:53:18 +0200

    extra 7 done

--- extra/extra_07/Program.cs
+++ extra/extra_07/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace extra_07
 {
@@ -8,6 +9,25 @@ namespace extra_07
     {
       // Add your code here:
 
+      int counter = 0;
+      // ask user for numbers/integers
+      Console.WriteLine("Give integers, 'end' quits:");
+      while (true)
+      {
+
+        // end loop on "end"
+        string input = Console.ReadLine();
+        if (input == "end")
+        {
+          break;
+        }
+        // if numbers collect them to counter and calculate sum
+        int number = Convert.ToInt32(input);
+        counter = number + counter;
+      }
+      // print the sum to console
+      Console.WriteLine("Sum: " + counter);
     }
   }
 }
+
