e38bb7260e73649ced11fb02cbc9af8a603f9fc9
markusbjorklund <markus.bjorklund@gmail.com>
2020-03-13 01:53:58 +0200

    looping loops

--- extra/extra_09/Program.cs
+++ extra/extra_09/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace extra_09
 {
@@ -8,6 +9,32 @@ namespace extra_09
     {
       // Add your code here:
 
+      // looping a list with different loops
+
+      List<string> list = new List<string>();
+      list.Add("This is a string");
+      list.Add("This is magic");
+      list.Add("THIS IS PATRICK!");
+
+      // for loop
+      Console.WriteLine("for loop");
+      for (int i = 0; i < list.Count; i++)
+      {
+        Console.WriteLine(list[i]);
+      }
+
+      // while loop
+      Console.WriteLine("\nwhile loop");
+      int index = 0;
+      while (index < list.Count)
+      {
+        Console.WriteLine(list[index]);
+        index++;
+      }
+
+      // foreach loop
+      Console.WriteLine("\nforeach loop");
+      list.ForEach(Console.WriteLine);
     }
   }
 }
