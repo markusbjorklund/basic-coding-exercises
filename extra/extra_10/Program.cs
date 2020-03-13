2bd9fa380d862085307db4187bc734195cee083d
markusbjorklund <markus.bjorklund@gmail.com>
2020-03-13 17:35:19 +0200

    number 10 dobe

--- extra/extra_10/Program.cs
+++ extra/extra_10/Program.cs
@@ -8,6 +8,25 @@ namespace extra_10
     {
       // Add your code here:
 
+      // print the array to stars
+      int[] array = { 5, 1, 3, 4, 2 };
+      PrintArrayInStars(array);
+    }
+
+    // define the method
+    public static void PrintArrayInStars(int[] array)
+    {
+      // call and list the array
+      for (int i = 0; i < array.Length; i++)
+      {
+        // transform numbers to stars
+        for (int stars = 0; stars < array[i]; stars++)
+        {
+          Console.Write("*");
+        }
+        // insert linebreaks for each row
+        Console.WriteLine();
+      }
     }
   }
 }
