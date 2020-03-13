90bd28ffd55ec4dfc08819446dfb4887ab5b806c
markusbjorklund <markus.bjorklund@gmail.com>
2020-03-13 21:21:52 +0200

    done

--- extra/extra_14/Program.cs
+++ extra/extra_14/Program.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.IO;
 
-namespace extra_14
+namespace extra_13
 {
   class Program
   {
     public static void Main(string[] args)
     {
       // Add your code here:
-
+      
+      // read text.txt and print to console
+      string text = File.ReadAllText("text.txt");
+      Console.WriteLine(text);
     }
   }
 }
