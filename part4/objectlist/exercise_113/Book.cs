5f96e30e866fa2479e4531744f06d21d74918f66
markusbjorklund <markus.bjorklund@gmail.com>
2020-03-15 22:14:29 +0200

    done

--- part4/objectlist/exercise_113/Book.cs
+++ part4/objectlist/exercise_113/Book.cs
@@ -21,28 +21,3 @@ namespace exercise_113
 
   }
 }
-
-// using System;
-
-// namespace exercise_113
-
-// {
-//     public class Book
-//     {
-//         public string title { get; }
-//         public int pages { get; set; }
-//         public int year { get; set; }
-
-//         public Book(string title, int pages, int year)
-//         {
-//             this.title = title;
-//             this.pages = pages;
-//             this.year = year;
-//         }
-
-//         public override string ToString()
-//         {
-//             return this.title + ", " + this.pages + " pages, " + this.year;
-//         }
-//     }
-// }
