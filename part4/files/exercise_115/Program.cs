b91e13580e3015e1fde81fd4e5bd5c1be205d047
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-03-16 15:50:41 +0200

    fixed

--- part4/files/exercise_115/Program.cs
+++ part4/files/exercise_115/Program.cs
@@ -2,22 +2,22 @@
 
 namespace exercise_115
 {
-    class Program
+  class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+      while (true)
+      {
+        string input = Console.ReadLine();
+        if (input == "end")
         {
-            while (true)
-            {
-                string input = Console.ReadLine();
-                if (input == "end")
-                {
-                    break;
-                }
-                // convert to numbers
-                int number = Convert.ToInt32(input);
-                // calculate
-                Console.WriteLine(number * number);
-            }
+          break;
         }
+        // convert to numbers
+        int number = Convert.ToInt32(input);
+        // calculate
+        Console.WriteLine(number * number * number);
+      }
     }
+  }
 }
