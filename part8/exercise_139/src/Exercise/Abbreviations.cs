97403ca0f4be07822f5e5aca1310c1d0286bd788
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-03-31 13:41:39 +0300

    139 done

--- part8/exercise_139/src/Exercise/Abbreviations.cs
+++ part8/exercise_139/src/Exercise/Abbreviations.cs
@@ -5,19 +5,45 @@ namespace Exercise
   public class Abbreviations
   {
 
+    // add dictionary for abbrevs
+    private Dictionary<string, string> abbRevs;
+
+    // add empty constructor
+    public Abbreviations()
+    {
+      this.abbRevs = new Dictionary<string, string>();
+    }
+
+    // add to abbrevs
     public void AddAbbreviation(string abbreviation, string explanation)
     {
-
+      this.abbRevs.Add(abbreviation, explanation);
     }
 
+    // check if abbrevs contain given string
     public bool HasAbbreviation(string abbreviation)
     {
-      return false;
+      if (this.abbRevs.ContainsKey(abbreviation))
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
     }
 
+    // return abbrev, if none return not found
     public string FindExplanationFor(string abbreviation)
     {
-      return null;
+      if (HasAbbreviation(abbreviation))
+      {
+        return abbRevs[abbreviation];
+      }
+      else
+      {
+        return "not found";
+      }
     }
   }
 }
