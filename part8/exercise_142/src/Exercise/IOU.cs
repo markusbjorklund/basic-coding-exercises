cebea34a36a2f0ae2c18e37fbab1e209c8abaa43
markusbjorklund <markus.bjorklund@gmail.com>
2020-04-02 13:26:53 +0300

    done

--- part8/exercise_142/src/Exercise/IOU.cs
+++ part8/exercise_142/src/Exercise/IOU.cs
@@ -4,45 +4,45 @@ namespace Exercise
 {
   public class IOU
   {
-    private Dictionary<string, int> directory;
+    private Dictionary<string, int> dictionary;
 
     // constructor public IOU() creates a new IOU
     public IOU()
     {
-      this.directory = new Dictionary<string, int>();
+      this.dictionary = new Dictionary<string, int>();
     }
 
     // saves the amount owed and the person owed to to the IOU. You can use this to also lower the debt, but the total amount cannot be negative!
     public void ChangeDebt(string toWhom, int amount)
     {
-      if (this.directory.ContainsKey(toWhom))
+      if (this.dictionary.ContainsKey(toWhom))
       {
-        int newAmount = this.directory[toWhom] + amount;
+        int newAmount = this.dictionary[toWhom] + amount;
         if (newAmount > 0)
         {
-          this.directory[toWhom] = newAmount;
+          this.dictionary[toWhom] = newAmount;
         }
         else
         {
-          this.directory[toWhom] = 0;
+          this.dictionary[toWhom] = 0;
         }
       }
       else if (amount > 0)
       {
-        this.directory[toWhom] = amount;
+        this.dictionary[toWhom] = amount;
       }
       else
       {
-        this.directory[toWhom] = 0;
+        this.dictionary[toWhom] = 0;
       }
     }
 
     // returns the amount owed to the person whose name is given as a parameter. If the person cannot be found, it returns 0.
     public int HowMuchDoIOweTo(string toWhom)
     {
-      if (this.directory.ContainsKey(toWhom))
+      if (this.dictionary.ContainsKey(toWhom))
       {
-        return this.directory[toWhom];
+        return this.dictionary[toWhom];
       }
       else
       {
