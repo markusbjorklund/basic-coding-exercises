50a062eb2920de0a0ff400bcf1e523bac483747c
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-04-06 13:29:24 +0300

    in progress

--- part8/exercise_146/src/Exercise/DictionaryOfManyTranslations.cs
+++ part8/exercise_146/src/Exercise/DictionaryOfManyTranslations.cs
@@ -5,16 +5,19 @@ namespace Exercise
   public class DictionaryOfManyTranslations
   {
 
+    // adds the translation for the word and preserves the old translations
     public void Add(string word, string translation)
     {
 
     }
 
+    // returns a list of the translations added for the word. If the word has no translations, the method should return an empty list
     public List<string> Translate(string word)
     {
       return new List<string>();
     }
 
+    // removes the word and all its translations from the dictionary. It's probably best to add the translations to an object variable that is of the type Dictionary<string, List<String> >
     public void Remove(string word)
     {
 
