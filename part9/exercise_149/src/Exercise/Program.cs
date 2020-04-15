1ff98d1728906d638b48946f7304914fbc0c3216
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-04-15 13:10:00 +0300

    149 done

--- part9/exercise_149/src/Exercise/Program.cs
+++ part9/exercise_149/src/Exercise/Program.cs
@@ -6,7 +6,24 @@ namespace Exercise
   {
     public static void Main(string[] args)
     {
+      Person ada = new Person("Ada Lovelace", "24 Maddox St. London W1S 2QN");
+      Person esko = new Person("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki");
+      Console.WriteLine(ada);
+      Console.WriteLine(esko);
 
+      Console.WriteLine();
+
+      Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
+      Console.WriteLine(ollie);
+      ollie.Study();
+      Console.WriteLine(ollie);
+
+      Console.WriteLine();
+
+      Teacher adaPoor = new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200);
+      Teacher eskoRich = new Teacher("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki", 5400);
+      Console.WriteLine(adaPoor);
+      Console.WriteLine(eskoRich);
     }
   }
 }
