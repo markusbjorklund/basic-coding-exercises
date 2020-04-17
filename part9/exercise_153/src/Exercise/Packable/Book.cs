e760306b30a1d01067842c93dadd19dc9a0e1cc8
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-04-17 11:26:05 +0300

    153 done

--- part9/exercise_153/src/Exercise/Packable/Book.cs
+++ part9/exercise_153/src/Exercise/Packable/Book.cs
@@ -2,19 +2,27 @@ namespace Exercise
 {
   public class Book : IPackable
   {
+    public string author { get; set; }
+    public string title { get; set; }
+    public int pubyear { get; set; }
+    public int weight;
 
-    public Book(string s, string s2, int i)
+    public Book(string author, string title, int pubyear)
     {
+      this.author = author;
+      this.title = title;
+      this.pubyear = pubyear;
+      this.weight = 1;
     }
 
     public int Weight()
     {
-      return 0;
+      return this.weight;
     }
 
     public override string ToString()
     {
-      return "";
+      return this.author + ": " + this.title + " (" + this.pubyear + ")";
     }
   }
 }
