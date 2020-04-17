9730a9da11f2cef96c1e4984eea6709c4924a2a7
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-04-17 12:21:13 +0300

    herd is done

--- part9/exercise_154/src/Exercise/Herd/Herd.cs
+++ part9/exercise_154/src/Exercise/Herd/Herd.cs
@@ -1,24 +1,36 @@
 namespace Exercise
 {
   using System.Collections.Generic;
+  using System;
   public class Herd : IMovable
   {
-
+    List<IMovable> herd;
     public Herd()
     {
+      this.herd = new List<IMovable>();
     }
 
-    public void AddToHerd(IMovable m)
+    public void AddToHerd(IMovable movable)
     {
+      this.herd.Add(movable);
     }
 
     public void Move(int dx, int dy)
     {
+      foreach (IMovable movable in herd)
+      {
+        movable.Move(dx, dy);
+      }
     }
 
     public override string ToString()
     {
-      return "";
+      string herdPosition = "";
+      foreach (IMovable movable in herd)
+      {
+        herdPosition += movable.ToString() + "\n";
+      }
+      return herdPosition;
     }
   }
 }
