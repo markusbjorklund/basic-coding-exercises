ac478a3fde4899d36f88983d2deca7135462d941
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-04-20 13:28:13 +0300

    159 done

--- part10/exercise_159/src/Exercise/Regex/Checker.cs
+++ part10/exercise_159/src/Exercise/Regex/Checker.cs
@@ -5,17 +5,41 @@ namespace Exercise
   {
     public bool DayOfWeek(string str)
     {
-      return false;
+      Regex regex = new Regex("^(mon|tue|wed|thu|fri|sat|sun)$");
+      if (regex.IsMatch(str))
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
     }
 
     public bool AllVowels(string str)
     {
-      return false;
+      Regex regex = new Regex("^(a|e|i|o|u)*$");
+      if (regex.IsMatch(str))
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
     }
 
     public bool TimeOfDay(string str)
     {
-      return false;
+      Regex regex = new Regex("^$|^(([01][0-9])|(2[0-3])):[0-5][0-9]:[0-5][0-9]$");
+      if (regex.IsMatch(str))
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
     }
   }
 }
