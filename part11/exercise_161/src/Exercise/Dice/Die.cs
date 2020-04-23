4a71a10a8360f3e1484ef2675933f39bcb136738
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-04-23 14:01:36 +0300

    161 done

--- part11/exercise_161/src/Exercise/Dice/Die.cs
+++ part11/exercise_161/src/Exercise/Dice/Die.cs
@@ -10,12 +10,15 @@ namespace Exercise
     {
       this.random = new Random();
       // Initialize the value of numberOfFaces here
+      this.numberOfFaces = numberOfFaces;
     }
+
     public int ThrowDie()
     {
       // generate a random number which may be any number
       // between one and the number of faces, and then return it
-      return 0;
+      int randomNumber = random.Next(1, numberOfFaces +1);
+      return randomNumber;
     }
   }
 }
