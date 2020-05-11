2c14c7bb5cfeba65b9faa1b0fdd8d7e157eca2a9
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-05-11 14:36:56 +0300

    fixed 170

--- part12/exercise_170/src/Exercise/Program.cs
+++ part12/exercise_170/src/Exercise/Program.cs
@@ -8,7 +8,6 @@ namespace Exercise
     {
       using (StreamWriter writer = new StreamWriter("file.txt"))
       {
-      // StreamWriter writer = new StreamWriter("file.txt");
       writer.WriteLine("Hello file!"); //writes the string "Hello file!" and line change to the file
       writer.WriteLine("More text");
       writer.Write("And a little extra"); // writes the string "And a little extra" to the file without a line change
