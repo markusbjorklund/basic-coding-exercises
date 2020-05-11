b425e4aa251e8673bfd43dbf7ece030dddadcfd4
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-05-11 14:41:04 +0300

    fixed 161

--- part11/exercise_161/src/Exercise/Dice/Die.cs
+++ part11/exercise_161/src/Exercise/Dice/Die.cs
@@ -17,17 +17,8 @@ namespace Exercise
     {
       // generate a random number which may be any number
       // between one and the number of faces, and then return it
-<<<<<<< HEAD
-      int randomNumber = random.Next(1, numberOfFaces +1);
+      int randomNumber = random.Next(1, this.numberOfFaces +1);
       return randomNumber;
-=======
-      int cast = random.Next(1, this.numberOfFaces + 1);
-
-      // numberOfFaces = 6
-      // Next(1,6)
-      // between 1 and 5
-      return cast;
->>>>>>> c41c326b51267b8a870cd7add7774b5bfb1e0b12
     }
   }
 }
